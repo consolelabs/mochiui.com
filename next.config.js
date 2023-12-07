3e0481b22eff665f63b09d42176131c9f487de64
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-07 15:49:00 +0700

    feat: config to build static page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
