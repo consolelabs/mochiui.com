7112b4913d0fdad524abc8a5ba85d500a94a46cd
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-07 15:28:22 +0700

    feat: scaffold template

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Doc{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
