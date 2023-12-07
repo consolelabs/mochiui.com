475124cf269e21bcd5695638a8a1f7e714643592
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-07 21:56:36 +0700

    fix: add base path for prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+const config = require('../next.config')
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -22,7 +24,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src={`${config.basePath}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -36,7 +38,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src={`${config.basePath}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
@@ -52,7 +54,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Doc{' '}
+            Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
