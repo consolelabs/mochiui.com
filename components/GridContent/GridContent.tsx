8ba364835c5d218213c8d9ece2cd14a9bb11e5c7
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-12-08 17:47:44 +0700

    feat: implement grid content and call to action (#1)

---
+++ components/GridContent/GridContent.tsx
@@ -0,0 +1,46 @@
+import { Typography } from '@mochi-ui/core'
+import clsx from 'clsx'
+
+type GridContentData = {
+  id?: string
+  icon: React.ReactNode
+  title: string
+  body: React.ReactNode
+}
+
+type GridContentProps = {
+  data: GridContentData[]
+  className?: string
+}
+
+export const GridContent = (props: GridContentProps) => {
+  const { data, className } = props
+  return (
+    <section className={clsx('px-4', className)}>
+      <div className="section-container flex flex-col md:flex-row justify-between items-start gap-12">
+        {data.map((d, index) => {
+          return (
+            <div
+              key={d?.id ?? index}
+              className="flex flex-col justify-center gap-3 p-0 w-full md:w-1/4 md:max-w-[216px] box-border"
+            >
+              <div className="flex flex-row md:flex-col gap-3 md:gap-6 items-center md:items-start">
+                {d.icon}
+                <Typography
+                  level="h6"
+                  fontWeight="lg"
+                  className="!text-neutral-800"
+                >
+                  {d.title}
+                </Typography>
+              </div>
+              <Typography level="p5" className="!text-neutral-600">
+                {d.body}
+              </Typography>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import { DesktopNav, TopBar, Button, Badge, MobileNav } from '@mochi-ui/core'
+import clsx from 'clsx'
+import { useEffect, useState } from 'react'
 import { Logo } from '../Logo'
 
 const desktopNavItems = [
@@ -58,23 +60,65 @@ const mobileNavItems = [
 ]
 
 export const Header = () => {
+  const [isScrolledToContent, setIsScrolledToContent] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // height of the Hero
+      if (window.pageYOffset > 500) {
+        setIsScrolledToContent(true)
+      } else {
+        setIsScrolledToContent(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div className="bg-transparent h-28 fixed top-0 z-50 bg-gradient-to-b from-gray-900 to-transparent w-full">
-      <TopBar
-        leftSlot={
-          <div className="flex items-center gap-4">
-            <Logo />
-            <Badge label="v1.0" className="!text-blue-300 !bg-blue-1000" />
-          </div>
-        }
-        rightSlot={
-          <>
-            <DesktopNav navItems={desktopNavItems} />
-            <MobileNav navItems={mobileNavItems} />
-          </>
-        }
-        className="!bg-transparent !max-w-8xl w-full mx-auto !border-none"
-      />
+    <div className="fixed top-0 z-50 w-full">
+      <div className="relative">
+        <div
+          className={clsx(
+            'h-24 w-full bg-gradient-to-b from-gray-900 to-transparent absolute top-0 transition-opacity duration-200 z-50',
+            {
+              'opacity-0': isScrolledToContent,
+              'opacity-100': !isScrolledToContent,
+            },
+          )}
+        />
+        <div
+          className={clsx(
+            'h-full w-full bg-gradient-to-b from-gray-900 to-gray-900 absolute top-0 transition-opacity duration-200 z-50',
+            {
+              'opacity-0': !isScrolledToContent,
+              'opacity-100': isScrolledToContent,
+            },
+          )}
+        />
+        <TopBar
+          leftSlot={
+            <div className="flex items-center gap-4">
+              <Logo />
+              <Badge label="v1.0" className="!text-blue-300 !bg-blue-1000" />
+            </div>
+          }
+          rightSlot={
+            <>
+              <DesktopNav navItems={desktopNavItems} />
+              <MobileNav
+                navItems={mobileNavItems}
+                toggleIconClassName="!text-white"
+              />
+            </>
+          }
+          className="!bg-transparent !max-w-8xl w-full mx-auto !border-none absolute top-0 z-50"
+        />
+      </div>
     </div>
   )
 }
