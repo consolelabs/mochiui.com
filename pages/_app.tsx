a0a3a45fde93fd86e1dc09553cc43c77f5630d96
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-27 16:29:46 +0700

    feat: use bigger og image and handle focus visible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import PlausibleProvider from 'next-plausible'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    async function loadExternalScript() {
+      // @ts-ignore
+      await import('focus-visible')
+    }
+
+    loadExternalScript()
+  }, [])
+
   return (
     <PlausibleProvider domain="mochiui.com">
       <Component {...pageProps} />
